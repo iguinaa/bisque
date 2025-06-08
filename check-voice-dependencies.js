4f23387bbc903be229c1a9979a3338e59bea8f67
andrew <andrew@macbook>
2025-06-08 11:21:53 -0400

    Cleanup of MacOS files and beginning removal of test files.

--- check-voice-dependencies.js
+++
@@ -1,5 +0,0 @@
-// Simple script to check voice dependencies
-import { generateDependencyReport } from '@discordjs/voice'
-
-console.log('=== Discord.js Voice Dependencies Report ===')
-console.log(generateDependencyReport())
